86db41abc4f8c174d4db2c396785d16b9fb3e2ad
JFT70 <94490112+JFT70@users.noreply.github.com>
2022-11-09 09:46:54 -0500

    Rename DZ60 layout 60_tsangan_split_rshift to 60_ansi_tsangan_split_rshift

--- keyboards/dz60/rules.mk
+++ keyboards/dz60/rules.mk
@@ -17,4 +17,4 @@ BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-LAYOUTS = 60_ansi 60_ansi_arrow_split_bs_7u_spc 60_ansi_arrow 60_ansi_split_bs_rshift 60_hhkb 60_iso 60_abnt2 60_tsangan_hhkb
+LAYOUTS = 60_ansi 60_ansi_arrow_split_bs_7u_spc 60_ansi_arrow 60_ansi_split_bs_rshift 60_hhkb 60_iso 60_abnt2 60_tsangan_hhkb 60_tsangan_split_rshift
