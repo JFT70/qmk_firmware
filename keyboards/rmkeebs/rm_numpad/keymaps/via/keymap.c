f34c2f3f3ceb1730cfa965887c0aba9c50f934ce
JFT70 <94490112+JFT70@users.noreply.github.com>
2023-07-07 15:57:46 -0400

    Made it so rm_numpad VIA keymap has 4 layers

--- keyboards/rmkeebs/rm_numpad/keymaps/via/keymap.c
+++ keyboards/rmkeebs/rm_numpad/keymaps/via/keymap.c
@@ -26,6 +26,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
         KC_P1,   KC_P2,   KC_P3,
            KC_P0,      KC_PDOT,    KC_PENT
+    ),
+
+    LAYOUT_numpad_6x4(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS,
+           KC_TRNS,    KC_TRNS,    KC_TRNS
+    ),
+
+    LAYOUT_numpad_6x4(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS,
+           KC_TRNS,    KC_TRNS,    KC_TRNS
+    ),
+
+    LAYOUT_numpad_6x4(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS,
+           KC_TRNS,    KC_TRNS,    KC_TRNS
     )
 
 };
